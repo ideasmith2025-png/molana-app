61ac40af6af2943408c094f4d13efe135599b372
ideasmith2025-png <ideasmith2025@gmail.com>
2025-12-23 18:44:56 +0330

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+const verses = [
+  {
+    type: "intro",
+    daftar: 1,
+    text: "مقدمه دفتر اول\nاین دفتر با نام خدا آغاز می‌شود.",
+    explain: ""
+  },
+  {
+    type: "verse",
+    daftar: 1,
+    number: 1,
+    text: "بشنو از نی چون حکایت می‌کند\nاز جدایی‌ها شکایت می‌کند",
+    explain: "آغاز مثنوی"
+  },
+  {
+    type: "section",
+    daftar: 1,
+    text: "بخش اول",
+    explain: "آغاز یک بخش تازه"
+  },
+  {
+    type: "verse",
+    daftar: 1,
+    number: 2,
+    text: "کز نیستان تا مرا ببریده‌اند\nاز نفیرم مرد و زن نالیده‌اند",
+    explain: ""
+  }
+];
