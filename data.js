15d86b143f201fdc221ee565a7afdf375af15e78
ideasmith2025-png <ideasmith2025@gmail.com>
2025-12-23 19:03:04 +0330

    Update data.js

--- data.js
+++ data.js
@@ -6,23 +6,23 @@ const verses = [
     explain: ""
   },
   {
+    type: "section",
+    daftar: 1,
+    text: "بخش اول",
+    explain: "آغاز نخستین بخش دفتر اول"
+  },
+  {
     type: "verse",
     daftar: 1,
     number: 1,
     text: "بشنو از نی چون حکایت می‌کند\nاز جدایی‌ها شکایت می‌کند",
-    explain: "آغاز مثنوی"
-  },
-  {
-    type: "section",
-    daftar: 1,
-    text: "بخش اول",
-    explain: "آغاز یک بخش تازه"
+    explain: "این بیت آغاز مثنوی است."
   },
   {
     type: "verse",
     daftar: 1,
     number: 2,
     text: "کز نیستان تا مرا ببریده‌اند\nاز نفیرم مرد و زن نالیده‌اند",
-    explain: ""
+    explain: "ادامه شرح فراق انسان."
   }
 ];
